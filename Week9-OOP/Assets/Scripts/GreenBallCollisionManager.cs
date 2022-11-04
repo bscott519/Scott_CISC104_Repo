fe57b4674a34942df593b536a9ab3e972fa02005
bscott519 <113139519+bscott519@users.noreply.github.com>
2022-11-03 21:45:01 -0400

    Week9-HW6

---
+++ Week9-OOP/Assets/Scripts/GreenBallCollisionManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+public class GreenBallCollisionManager : BallCollisionManager
+{
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Green Ball Collision Manager Function");
+
+        // Homework: Do something interesting here
+        // Green Ball gets bigger when colliding with Blue Ball
+
+        if (OtherBall.GetComponent<BlueBallCollisionManager>() != null)
+        {
+            Debug.Log("Green Ball Collided With Blue Ball");
+
+            transform.localScale += new Vector3(1, 1, 1);
+        }
+            
+    }
+}
+
---
+++ Week9-OOP/Assets/Scripts/RedBallCollisionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+public class RedBallCollisionManager : BallCollisionManager
+{
+    public AudioSource audioPlayer;
+   
+    public override void CollideWithBall(GameObject OtherBall)
+    {
+        Debug.Log("Red Ball Collision Manager Function");
+
+        // Homework: Do something interesting here
+        // Red Ball makes a collision sound when colliding with Yellow Ball
+
+        if (OtherBall.GetComponent<YellowBallCollisionManager>() != null)
+        {
+            Debug.Log("Red Ball Collided With Yellow Ball");
+
+            audioPlayer.Play();
+        }
+    }
+}
+
