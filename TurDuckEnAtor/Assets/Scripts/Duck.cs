58880986a4243f69bb3acb8395e3aaba156af805
bscott519 <113139519+bscott519@users.noreply.github.com>
2022-11-17 00:27:45 -0500

    Week11-TurDuckEn

---
+++ TurDuckEnAtor/Assets/Scripts/Duck.cs
@@ -0,0 +1,33 @@
+﻿using System;
+public class Duck : Bird
+{
+    /// <summary>
+    /// Do not call this method.  The default constructor throws an exception because
+    /// the duck needs a weight.
+    /// </summary>
+    /// <exception cref="NotSupportedException"></exception>
+	public Duck()
+    {
+        throw new NotSupportedException();
+    }
+
+    public Duck(int WeightInOunces) : base()
+	{
+        base.WeightInOunces = WeightInOunces;
+    }
+
+    public override void RemoveBonesAndAdjustWeight()
+    {
+        if (this.HasBones)
+        {
+            this.WeightInOunces -= (int)((float)this.WeightInOunces * .28);
+            this.HasBones = false;
+        }
+    }
+
+    public override int GetCalories()
+    {
+        return this.WeightInOunces * 96;
+    }
+}
+
