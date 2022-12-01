7ef08865ff719f57011883fc1cbaa91d6c93388b
bscott519 <113139519+bscott519@users.noreply.github.com>
2022-12-01 17:05:37 -0500

    Update UnoGame.cs

--- CISC 104 Project 2/Assets/Scripts/UnoGame.cs
+++ CISC 104 Project 2/Assets/Scripts/UnoGame.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class UnoGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Cards[] cards;
 }
