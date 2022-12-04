5fa10f3841bfab7e828d3d8f05bba064e4124bda
bscott519 <113139519+bscott519@users.noreply.github.com>
2022-12-03 23:01:50 -0500

    Updated Scripts and Scene

--- CISC 104 Project 2/Assets/Scripts/Cards.cs
+++ CISC 104 Project 2/Assets/Scripts/Cards.cs
@@ -2,17 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum CardDeck
+{
+    RedZero,
+    RedOne,
+    RedTwo,
+    RedThree,
+    RedFour,
+    RedFive,
+    RedSix,
+    RedSeven,
+    RedEight,
+    RedNine,
+    BlueZero,
+    BlueOne,
+    BlueTwo,
+    BlueThree,
+    BlueFour,
+    BlueFive,
+    BlueSix,
+    BlueSeven,
+    BlueEight,
+    BlueNine,
+    YellowZero,
+    YellowOne,
+    YellowTwo,
+    YellowThree,
+    YellowFour,
+    YellowFive,
+    YellowSix,
+    YellowSeven,
+    YellowEight,
+    YellowNine,
+    GreenZero,
+    GreenOne,
+    GreenTwo,
+    GreenThree,
+    GreenFour,
+    GreenFive,
+    GreenSix,
+    GreenSeven,
+    GreenEight,
+    GreenNine,
+    RedSkipTurn,
+    BlueSkipTurn,
+    YellowSkipTurn,
+    GreenSkipTurn,
+    RedMinus2,
+    BlueMinus2,
+    YellowMinus2,
+    GreenMinus2,
+
+}
+
 public class Cards : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
--- CISC 104 Project 2/Assets/Scripts/UnoGame.cs
+++ CISC 104 Project 2/Assets/Scripts/UnoGame.cs
@@ -1,8 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class UnoGame : MonoBehaviour
 {
     public Cards[] cards;
+    private int currentCardIndex;
+    public GameObject cardTextGameObject;
+    private TextMeshProUGUI cardText;
+
+    void Start()
+    {
+        currentCardIndex = 0;
+        cardText = cardTextGameObject.GetComponent<TextMeshProUGUI>();
+
+    }
+
+    void PreviousCard()
+    {
+        if(currentCardIndex == 0)
+        {
+            currentCardIndex = cards.Length - 1;
+        }
+        else
+        {
+            currentCardIndex--;
+        }
+
+    }
+
+    void NextCard()
+    {
+        if (currentCardIndex == 0)
+        {
+            currentCardIndex = cards.Length - 1;
+        }
+        else
+        {
+            currentCardIndex++;
+        }
+
+    }
 }
