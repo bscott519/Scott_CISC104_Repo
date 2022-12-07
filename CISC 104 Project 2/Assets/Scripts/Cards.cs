9f0204483b198ee16907dcf5588a41092a2cfaaa
StudiousKodak <Nicj609@Gmail.com>
2022-12-06 22:41:01 -0500

    project 2
    updated some stuff, message me if anything needs to clarified or broke :)

--- CISC 104 Project 2/Assets/Scripts/Cards.cs
+++ CISC 104 Project 2/Assets/Scripts/Cards.cs
@@ -5,68 +5,23 @@ using UnityEngine.UI;
 
 public enum CardSuit
 {
-    RedZero,
-    RedOne,
-    RedTwo,
-    RedThree,
-    RedFour,
-    RedFive,
-    RedSix,
-    RedSeven,
-    RedEight,
-    RedNine,
-    BlueZero,
-    BlueOne,
-    BlueTwo,
-    BlueThree,
-    BlueFour,
-    BlueFive,
-    BlueSix,
-    BlueSeven,
-    BlueEight,
-    BlueNine,
-    YellowZero,
-    YellowOne,
-    YellowTwo,
-    YellowThree,
-    YellowFour,
-    YellowFive,
-    YellowSix,
-    YellowSeven,
-    YellowEight,
-    YellowNine,
-    GreenZero,
-    GreenOne,
-    GreenTwo,
-    GreenThree,
-    GreenFour,
-    GreenFive,
-    GreenSix,
-    GreenSeven,
-    GreenEight,
-    GreenNine,
-    RedSkipTurn,
-    BlueSkipTurn,
-    YellowSkipTurn,
-    GreenSkipTurn,
-    RedMinus2,
-    BlueMinus2,
-    YellowMinus2,
-    GreenMinus2,
-    RedMinus4,
-    BlueMinus4,
-    YellowMinus4,
-    GreenMinus4
+    // Base Cards
+    RedZero, RedOne, RedTwo, RedThree, RedFour, RedFive, RedSix, RedSeven, RedEight, RedNine,
+    BlueZero, BlueOne, BlueTwo, BlueThree, BlueFour, BlueFive, BlueSix, BlueSeven, BlueEight, BlueNine,
+    YellowZero, YellowOne,  YellowTwo, YellowThree, YellowFour, YellowFive, YellowSix, YellowSeven, YellowEight, YellowNine,
+    GreenZero, GreenOne, GreenTwo, GreenThree, GreenFour, GreenFive, GreenSix, GreenSeven, GreenEight, GreenNine,
+
+    // Special Cards
+    RedSkipTurn, BlueSkipTurn, YellowSkipTurn, GreenSkipTurn,
+    RedMinus2, BlueMinus2, YellowMinus2, GreenMinus2,
+    RedMinus4, BlueMinus4, YellowMinus4, GreenMinus4
 }
 
 public class Cards : MonoBehaviour
 {
-    public string CardName;
-
-    public RawImage cardImage;
-
-    public int bigness;
-
-    public CardSuit cardSuit;
-
+    public string CardName;     // Name
+    public CardSuit cardSuit;   // Card
+    public int value;           // Number
+    public string color;        // Color
+    public int specialFlag;     // 0 = nothing 1 = has skip/minus
 }
--- CISC 104 Project 2/Assets/Scripts/UnoGame.cs
+++ CISC 104 Project 2/Assets/Scripts/UnoGame.cs
@@ -7,25 +7,42 @@ using UnityEngine.UI;
 public class UnoGame : MonoBehaviour
 {
     public Cards[] cards;
-    public RawImage currentCardImage;
+
+    public Cards[] userHand;
+    public Cards[] computer1Hand;
+    public Cards[] computer2Hand;
+    public Cards[] usedCards;
+
     private int currentCardIndex;
+    private int playerCardCount;
+    private int c1CardCount;
+    private int c2CardCount;
+
     public GameObject cardTextGameObject;
     private TextMeshProUGUI cardText;
 
     public GameObject winnerTextObject;
-
     private TextMeshProUGUI winnerText;
 
 
+    // The inital setup (not the actual startbutton)
     void Start()
     {
         currentCardIndex = 0;
         cardText = cardTextGameObject.GetComponent<TextMeshProUGUI>();
-        SetImage();
+        
 
         winnerText = winnerTextObject.GetComponent<TextMeshProUGUI>();
     }
 
+    // Updated once a frame
+    void Update()
+    {
+        winnerText.text = "Player #1 Wins!";
+    }
+
+    // cycles hand to the left 1
+    // add a check for boundaries (!<0)
     public void PreviousCard()
     {
         if(currentCardIndex == 0)
@@ -36,9 +53,10 @@ public class UnoGame : MonoBehaviour
         {
             currentCardIndex--;
         }
-        SetImage();
     }
 
+    // cycles hand to the right 1
+    // add a check for boundaries (length)
     public void NextCard()
     {
         if(currentCardIndex == 0)
@@ -49,40 +67,28 @@ public class UnoGame : MonoBehaviour
         {
             currentCardIndex++;
         }
-        SetImage();
-    }
-    
-    private void SetImage()
-    {
-        cardText.text = cards[currentCardIndex].CardName;
-
-        if (cards[currentCardIndex].cardImage != null)
-        {
-            currentCardImage.texture = cards[currentCardIndex].cardImage.texture;
-        }
-        else
-        {
-            currentCardImage.texture = null;
-        }
     }
 
-    void Update()
-    {
-        winnerText.text = "Player #1 Wins!";
-    }
-
+    // Play a card
     public void PlayButton()
     {
-        
+        // remove card from user hand if you have a card to play
+        // if no available card button is diabled
     }
 
+    // Draw a card
     public void DrawButton()
     {
+        // continue drawing card until either color or number match
+        // disabled when you do have a card
         currentCardIndex++;
     }
 
+    // Start And Restart Game
     public void RestartButton()
     {
+        // Begins with text saying 'start', after pressed
+        // Text changes to 'restart', allowing the user to restart at anypoint during the game and reset everything
         currentCardIndex = 0;
     }
 }
