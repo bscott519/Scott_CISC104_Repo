608a23f401ebf5ae73d28c0753b80320040a2008
bscott519 <113139519+bscott519@users.noreply.github.com>
2023-02-08 16:21:23 -0500

    CISC 204 my project
    Added CISC 204 my project and renamed CISC104 my project "My project 1"

---
+++ My project/Assets/Scripts/TextAssignmentComponent.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextAssignmentComponent : MonoBehaviour
+{
+    public GameObject nameTextGameObject;
+    public GameObject udidTextGameObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TextMeshPro nameText = nameTextGameObject.GetComponent<TextMeshPro>();
+        TextMeshPro udidText = udidTextGameObject.GetComponent<TextMeshPro>();
+
+        // Fill in this string with your name.
+        nameText.text = "Benjamin Scott";
+        udidText.text = SystemInfo.deviceUniqueIdentifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Nothing happens here...yet.
+    }
+}
